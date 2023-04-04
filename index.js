5a218ddda8ea90263bd7364416b0f42c7d76f42f
tarunaNangla <tarunanangla131@gmail.com>
2023-04-04 12:39:40 +0530

    done

---
+++ index.js
@@ -0,0 +1,34 @@
+// console.log("Hello Sonia")
+
+//adding express for creating backend framework;
+const express= require("express");
+const app=express();
+
+
+//adding other important libraries;
+const dotenv=require("dotenv");
+const morgan=require("morgan");
+const helmet=require("helmet");
+const mongoose=require("mongoose");
+const userRoute=require("./routes/user");
+const authRoute=require("./routes/auth")
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
+
+
+//middleware
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"))
+ 
+
+//routing
+app.use("/api/users",userRoute)
+app.use("/api/auth",authRoute)
+
+
+app.listen(8801,()=>{
+    console.log("Backend port is ready");
+})
